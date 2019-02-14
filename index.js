4559a842cff10798a166621d9ec66b361dbb8005
jkbraeckel <42634854+jkbraeckel@users.noreply.github.com>
2019-02-14 20:16:33 +1300

    #4 keep posts when server restarts
    keep posts when server restarts

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //set up
+let databasePosts = null;
 let express = require('express')
 let app = express();
 let bodyParser = require('body-parser')
@@ -25,9 +26,24 @@ function saveNewPost(request, response) {
   console.log(request.body.message); //write it on the command prompt so we can see
   posts.push(request.body.message); //save it in our list
   response.send("thanks for your message. Press back to add another");
+  databasePosts.insert(post);
 }
 app.post('/posts', saveNewPost);
+let MongoClient = require('mongodb').MongoClient;
+let databaseUrl = 'mongodb://girlcode2019:hats123@ds135305.mlab.com:35305/girlcode2019level2';
+let databaseName = 'girlcode2019level2';
 
+MongoClient.connect(databaseUrl, {useNewUrlParser: true}, function(err, client) {
+  if (err) throw err;
+  console.log("yay we connected to the database");
+  let database = client.db(databaseName);
+  databasePosts = database.collection('posts');
+  databasePosts.find({}).toArray(function(err, results) {
+    if (err) throw err;
+    console.log("Found " + results.length + " results");
+    posts = results
+  });
+});
 //listen for connections on port 3000
 app.listen(3000);
 console.log("Hi! I am listening at http://localhost:3000");
